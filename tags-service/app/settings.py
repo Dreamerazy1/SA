497fa62f156196316c017ed745d86986e344e351
Yevgeniy Kim <evgeshkakeshka024@gmail.com>
2025-11-14 00:53:15 +0500

    rendertest

--- tags-service/app/settings.py
+++ tags-service/app/settings.py
@@ -3,8 +3,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # MongoDB connection URL
+    # For local: mongodb://localhost:27017
+    # For Atlas: mongodb+srv://username:password@cluster.mongodb.net/database?retryWrites=true&w=majority
     mongodb_url: str = "mongodb://localhost:27017"
     mongodb_database: str = "tags_db"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 @lru_cache()
